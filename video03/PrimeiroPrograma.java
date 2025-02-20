da3b8f3423a44a7a23e619941c61361ceceeae11
Emerson Paduan <emerson@paduan.dev.br>
2025-02-20 15:38:49 -0300

    video03

---
+++ video03/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+// Comentário de linha
+
+/* 
+   Comentário de bloco
+   
+   Atalhos: class, main, sout
+
+*/
+
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Meu Primeiro Programa");
+    }
+}
