cc96d7569416982a65fd5de08e0cbb695c5e56eb
Emerson Paduan <emerson@paduan.dev.br>
2025-02-21 10:16:56 -0300

    video 06

---
+++ video06/Exemplo02.java
@@ -0,0 +1,10 @@
+public class Exemplo02 {
+    public static void main(String[] args) {
+        System.out.println(10 > 2);
+        System.out.println(2 > 10);
+        System.out.println(10 > 10);
+        System.out.println(10 >= 10);
+        System.out.println(10 != 10); // warnning
+        System.out.println(11 != 10);
+    }
+}
