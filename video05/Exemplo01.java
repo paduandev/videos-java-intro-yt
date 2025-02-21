c223e99f4e4aca957986d9063d0c0642cd494346
Emerson Paduan <emerson@paduan.dev.br>
2025-02-20 23:49:31 -0300

    video 05

---
+++ video05/Exemplo01.java
@@ -0,0 +1,9 @@
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+        // System.out.println("Olá"); // ln = new line
+        // System.out.println("Boa noite");
+
+        System.out.println("Olá\nBoa\nnoite"); // '\n' quebra de linha
+    }
+}
---
+++ video05/Exemplo03.java
@@ -0,0 +1,7 @@
+public class Exemplo03 {
+    public static void main(String[] args) {
+        System.out.println("Resultado = " + 10);
+        System.out.println("Resultado = " + 10 + 10);
+        System.out.println("Resultado = " + (10 + 10));
+    }
+}
